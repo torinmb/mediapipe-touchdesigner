5b4cc6fb9eaa9f3c8c0b850467f78efa7b5c2143
cardstotheheart@gmail.com <36455793+domisjustanumber@users.noreply.github.com>
2023-08-14 18:10:40 -0400

    Now load files in webserver startup

--- td_scripts/Media_Pipe/mp_webserver_callbacks.py
+++ td_scripts/Media_Pipe/mp_webserver_callbacks.py
@@ -1,4 +1,7 @@
 import mimetypes
+import os
+from pathlib import Path
+import shutil
 
 # me - this DAT.
 # webServerDAT - the connected Web Server DAT
@@ -57,6 +60,36 @@ def onServerStart(webServerDAT):
 	print("Loading MIME types")
 	mimetypes.add_type('application/octet-stream', 'task')
 	mimetypes.add_type('application/octet-stream', 'tflite')
+	importRoot = os.path.join(os.getcwd(), 'dist')
+
+	if(os.path.exists(importRoot)):
+		print("Importing files from: " + importRoot)
+		vfsOp = op('virtualFile')
+		vfiles = []
+
+		print("Found " + str(len(vfsOp.vfs)) + " virtual files")
+
+		# Get all the virtual files
+		for f in vfsOp.vfs:
+			print(f.name)
+			vfiles.append(f.name)
+
+		# Delete all the virtual files
+		# # (don't do this as you iterate above, it doesn't work in TD 2022.33910 )
+		for v in vfiles:
+			vfsOp.vfs[v].destroy()
+
+		for filename in Path(importRoot).rglob('*'):
+			if (filename.is_file()):
+				file_path = filename.relative_to(importRoot)
+				vfsFilename = "#".join(file_path.parts)
+				print("Found: "+ vfsFilename)
+				vfsOp.vfs.addFile(filename, overrideName="#"+vfsFilename)
+		try:
+			shutil.rmtree(importRoot)
+			print(importRoot + " removed successfully")
+		except OSError as o:
+			print(f"Error, {o.strerror}: {importRoot}")
 	return
 
 def onServerStop(webServerDAT):
