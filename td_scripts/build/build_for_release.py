31931e7986fd4e93c8c5150e1a1facdbe861e8ae
cardstotheheart@gmail.com <36455793+domisjustanumber@users.noreply.github.com>
2023-08-15 15:53:21 -0400

    Deletes and makes a release folder, exports the MP tox there

--- td_scripts/build/build_for_release.py
+++ td_scripts/build/build_for_release.py
@@ -31,7 +31,19 @@ def onStart():
 def onCreate():
 	clear()
 
+	releaseFolder = 'release'
 	distFolder = '_mpdist'
+	mpOp = op('/project1/MediaPipe')
+	vfsOp = op('/project1/MediaPipe/virtualFile')
+
+	print("Removing and recreating existing release dir")
+	try:
+		shutil.rmtree(releaseFolder)
+		print(str(releaseFolder) + " removed successfully")
+	except OSError as o:
+		print(f"Error, {o.strerror}: {releaseFolder}")
+
+	Path(releaseFolder).mkdir()
 	print("Initing yarn build")
 	
 	# Specify the path to the desired directory
@@ -39,31 +51,41 @@ def onCreate():
 
 	# Run the command in the specified directory
 	subprocess.check_call(["yarn", "build"],shell=True, cwd=directory_path)
-	mpOp = op('/project1/MediaPipe')
-	vfsOp = op('/project1/MediaPipe/virtualFile')
-
-	importRoot = os.path.join(os.getcwd(), distFolder)
+	
+	importRoot = directory_path.joinpath(distFolder)
 	purgeVFS(vfsOp)
-	print("Checking for new files at: " + importRoot)
+	print("Checking for new files at: " + str(importRoot))
 
 	if(os.path.exists(importRoot)):
-		print("Importing files from: " + importRoot)
+		print("Importing files from: " + str(importRoot))
 		for filename in Path(importRoot).rglob('*'):
 			if (filename.is_file()):
 				file_path = filename.relative_to(importRoot)
 				vfsFilename = "#".join(file_path.parts)
 				print("Importing: "+ vfsFilename)
 				vfsOp.vfs.addFile(filename, overrideName="#"+vfsFilename)
+		
+		# Save the original MediaPipe tox path
+		originalToxPath = mpOp.par.externaltox.eval()
+		print("Current external tox path: "+originalToxPath)
+		
+		# Keep the filename, but move the location to our release folder
+		e = Path(originalToxPath)
+		existingName = e.name
+		mpOp.par.externaltox = (str(directory_path.joinpath(releaseFolder, existingName)))
 		if(mpOp.saveExternalTox(recurse=False)):
 			print("***** Saved tox *****")
 			purgeVFS(vfsOp)
 			try:
 				shutil.rmtree(importRoot)
-				print(importRoot + " removed successfully")
+				print(str(importRoot) + " removed successfully")
 			except OSError as o:
 				print(f"Error, {o.strerror}: {importRoot}")
 		else:
 			print("Failed to save tox, aborting")
+
+		# Put our tox path back into the MediaPipe COMP
+		mpOp.par.externaltox = originalToxPath
 	return
 
 def onExit():
