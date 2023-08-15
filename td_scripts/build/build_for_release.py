8015e72d71ae7cfa059001d7b3452f3c29cfc41e
cardstotheheart@gmail.com <36455793+domisjustanumber@users.noreply.github.com>
2023-08-15 15:24:56 -0400

    Now runs yarn build first

--- td_scripts/build/build_for_release.py
+++ td_scripts/build/build_for_release.py
@@ -1,3 +1,4 @@
+import subprocess
 import os
 from pathlib import Path
 import shutil
@@ -11,7 +12,7 @@ import shutil
 
 def purgeVFS(op):
 	vfiles = []
-	print("Found " + str(len(op.vfs)) + " virtual files")
+	print("Found " + str(len(op.vfs)) + " virtual file(s) for deletion")
 
 	# Get all the virtual files
 	for f in op.vfs:
@@ -28,33 +29,41 @@ def onStart():
 	return
 
 def onCreate():
+	clear()
+
 	distFolder = '_mpdist'
+	print("Initing yarn build")
+	
+	# Specify the path to the desired directory
+	directory_path = Path.cwd()
+
+	# Run the command in the specified directory
+	subprocess.check_call(["yarn", "build"],shell=True, cwd=directory_path)
+	mpOp = op('/project1/MediaPipe')
 	vfsOp = op('/project1/MediaPipe/virtualFile')
 
 	importRoot = os.path.join(os.getcwd(), distFolder)
-	purgeVFS(vfsOp.vfs)
+	purgeVFS(vfsOp)
 	print("Checking for new files at: " + importRoot)
 
 	if(os.path.exists(importRoot)):
 		print("Importing files from: " + importRoot)
-		
-
 		for filename in Path(importRoot).rglob('*'):
 			if (filename.is_file()):
 				file_path = filename.relative_to(importRoot)
 				vfsFilename = "#".join(file_path.parts)
 				print("Importing: "+ vfsFilename)
 				vfsOp.vfs.addFile(filename, overrideName="#"+vfsFilename)
-		if(parent().saveExternalTox(recurse=False)):
-			print("Saved tox")
-			purgeVFS(vfsOp.vfs)
+		if(mpOp.saveExternalTox(recurse=False)):
+			print("***** Saved tox *****")
+			purgeVFS(vfsOp)
 			try:
 				shutil.rmtree(importRoot)
 				print(importRoot + " removed successfully")
 			except OSError as o:
 				print(f"Error, {o.strerror}: {importRoot}")
 		else:
-			print("Failed to save tox")
+			print("Failed to save tox, aborting")
 	return
 
 def onExit():
