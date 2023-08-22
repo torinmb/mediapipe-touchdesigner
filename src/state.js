6b53f766660128a9815ddbd3ae635398cb11cfc2
cardstotheheart@gmail.com <36455793+domisjustanumber@users.noreply.github.com>
2023-08-22 18:26:39 -0400

    Only change if the new camera is not the old camera

--- src/state.js
+++ src/state.js
@@ -35,50 +35,53 @@ export let outputState = {
 }
 
 async function changeWebcam(webcam) {
-  const devices = await navigator.mediaDevices.enumerateDevices();
-  const webcamDevices = devices.filter(device => device.kind === 'videoinput');
-  console.log("Got webcams: ", webcamDevices);
-  webcamDevices.forEach((webcamDevice) => {
-    if (webcamDevice.label == webcam) {
-      webcamState.webcamId = webcamDevice.deviceId;
-    }
-    //  console.log(webcam.label + " capabilities:", webcam.getCapabilities());
-  });
-
-  const constraints = {
-    video: {
-      deviceId: {
-        exact: webcamState.webcamId,
-      },
-      height: {
-        exact: webcamState.height,
-      },
-      frameRate: {
-        ideal: webcamState.targetFrameRate,
+  if (webcam !== webcamState.webcamLabel) {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const webcamDevices = devices.filter(device => device.kind === 'videoinput');
+    // console.log("Got webcams: ", webcamDevices);
+    webcamDevices.forEach((webcamDevice) => {
+      if (webcamDevice.label == webcam) {
+        webcamState.webcamId = webcamDevice.deviceId;
       }
+      //  console.log(webcam.label + " capabilities:", webcam.getCapabilities());
+    });
+
+    const constraints = {
+      video: {
+        deviceId: {
+          exact: webcamState.webcamId,
+        },
+        height: {
+          exact: webcamState.height,
+        },
+        frameRate: {
+          ideal: webcamState.targetFrameRate,
+        }
+      }
+    };
+
+    // Stop the old webcam stream
+    if (webcamState.webcamRunning) {
+      const tracks = webcamState.videoElement.srcObject.getTracks();
+      tracks.forEach((track) => {
+        track.stop();
+      });
     }
-  };
 
-  // Stop the old webcam stream
-  if (webcamState.webcamRunning) {
-    const tracks = webcamState.videoElement.srcObject.getTracks();
-    tracks.forEach((track) => {
-      track.stop();
-    });
+    // Try and start a new one
+    try {
+      let stream = await navigator.mediaDevices.getUserMedia(constraints);
+      webcamState.videoElement.srcObject = stream;
+      webcamState.webcamRunning = true;
+      webcamState.webcamLabel = webcam;
+      stream.getTracks().forEach(function (track) {
+        let trackSettings = track.getSettings();
+        webcamState.frameRate = trackSettings.frameRate;
+        console.log("Webcam settings: ", trackSettings);
+      });
+    } catch (err) {
+      console.log(err.name + ": " + err.message);
+    }
+    webcamState.videoElement.height = webcamState.height;
   }
-
-  // Try and start a new one
-  try {
-    let stream = await navigator.mediaDevices.getUserMedia(constraints);
-    webcamState.videoElement.srcObject = stream;
-    webcamState.webcamRunning = true;
-    stream.getTracks().forEach(function (track) {
-      let trackSettings = track.getSettings();
-      webcamState.frameRate = trackSettings.frameRate;
-      console.log("Webcam settings: ", trackSettings);
-    });
-  } catch (err) {
-    console.log(err.name + ": " + err.message);
-  }
-  webcamState.videoElement.height = webcamState.height;
 }
