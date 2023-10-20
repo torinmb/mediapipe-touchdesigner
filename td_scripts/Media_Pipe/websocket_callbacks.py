77e83e897f1c466f1d7d940720a8dacfd7fa3ca4
Dom Scott <36455793+domisjustanumber@users.noreply.github.com>
2023-10-20 15:16:05 -0400

    Reload web browser after 3 secs if webcam fails to load

--- td_scripts/Media_Pipe/websocket_callbacks.py
+++ td_scripts/Media_Pipe/websocket_callbacks.py
@@ -34,6 +34,13 @@ def onReceiveText(dat, rowIndex, message):
 		data = json.loads(message)
 		if 'type' in data:
 			op('webcam_list').text = json.dumps(data['devices'])
+		
+		if 'error' in data:
+			if (data['error'] == 'webcamStartFail'):
+				print('Webcam failed to start, please ensure camera input is 720p, 8 bit. Reloading component')
+				op('webBrowser1').allowCooking = False
+				op('webBrowser1').par.Address = op('current_url').text
+				run("me.parent().op('webBrowser1').allowCooking = True", delayMilliSeconds = 3000)
 
 	except Exception as e:
 		return
