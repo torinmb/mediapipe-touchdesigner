bd353b8f0a5f5fbc5679ba16bce01fa3d552e0ec
Dom Scott <36455793+domisjustanumber@users.noreply.github.com>
2023-11-10 10:26:48 -0500

    MediaPipe 0.10.8

---
+++ src/mediapipe/tasks-vision/0.10.8/wasm/vision_wasm_internal.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f5ba7b1d0adad63c581a80113567913a7106b20f8d26982f82c56998c7d44465
+size 203247
---
+++ src/mediapipe/tasks-vision/0.10.8/wasm/vision_wasm_nosimd_internal.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:731786df74b19150eecc8fe69ddf16040bbbba8cf2d22c964ef38ecef25d1e1f
+size 203156
