281b024b7478785d88621936e5e295759a9df695
Dom Scott <dom@domscott.ca>
2024-12-03 16:36:58 -0500

    Working with 2D irises, but still z inverted

--- td_scripts/face_tracking/landmarks_to_SOP_callbacks.py
+++ td_scripts/face_tracking/landmarks_to_SOP_callbacks.py
@@ -19,6 +19,8 @@ def onPulse(par):
 
 def onCook(scriptOp):
 	scriptOp.clear()
+	irisTable = op('irises'+str(scriptOp.digits))
+
 	if(op('in1').text != ""):
 		rawdata = json.loads(op('in1').text)
 		if not len(rawdata):
@@ -32,10 +34,38 @@ def onCook(scriptOp):
 			landmarks = rawdata['faceLandmarkResults']['faceLandmarks'][digits]
 
 			# For every point we have, edit the position of the existing face points
+			# Stop when we reach the irises (point 468)
 			i=0
-			for eachPoint in scriptOp.points:
-				eachPoint.P = (landmarks[i]['x'], -1*(landmarks[i]['y'] * resScale)+.78, landmarks[i]['z'])
+			while i<468:
+				scriptOp.points[i].P = (landmarks[i]['x'], -1*(landmarks[i]['y'] * resScale)+.78, landmarks[i]['z'])
 				i += 1
+		# 	# Right iris
+			op(irisTable)[1,0] = "Right iris center"
+			op(irisTable)[1,1] = landmarks[468]['x']
+			op(irisTable)[1,2] = -1*(landmarks[468]['y'] * resScale)+.78
+			op(irisTable)[1,3] = landmarks[468]['z']
+		
+			op(irisTable)[2,0] = "Right iris radius"
+			op(irisTable)[2,1] = abs(landmarks[469]['x'] - landmarks[471]['x']) / 2
+			op(irisTable)[2,2] = abs(landmarks[470]['y'] - landmarks[472]['y']) / 2 * resScale
+
+			op(irisTable)[3,0] = "Left iris center"
+			op(irisTable)[3,1] = landmarks[473]['x']
+			op(irisTable)[3,2] = -1*(landmarks[473]['y'] * resScale)+.78
+			op(irisTable)[3,3] = landmarks[473]['z']
+
+
+			op(irisTable)[4,0] = "Left iris radius"
+			op(irisTable)[4,1] = abs(landmarks[474]['x'] - landmarks[476]['x']) / 2
+			op(irisTable)[4,2] = abs(landmarks[475]['y'] - landmarks[477]['y']) / 2 * resScale
+
 		else:
 			# print("no face")
+			op(irisTable).clear()
+			# op(irisTable).setSize(5, 4)
+			op(irisTable).insertRow(["Description","x","y","z"])
+			op(irisTable).appendRow(["Right iris center",0,0,0])
+			op(irisTable).appendRow(["Right iris radius",0,0,0])
+			op(irisTable).appendRow(["Left iris center",0,0,0])
+			op(irisTable).appendRow(["Left iris radius",0,0,0])
 			return
