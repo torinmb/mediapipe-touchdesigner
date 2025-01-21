6f8f63c23ea96795d24b1e303e05975d0965189f
Torin Blankensmith <torin.blankensmith@gmail.com>
2025-01-20 20:57:27 -0800

    if MP exists at root or /toxes/MediaPipe.tox set path and enable external

--- td_scripts/Media_Pipe/external_tox_check.py
+++ td_scripts/Media_Pipe/external_tox_check.py
@@ -4,6 +4,7 @@
 # state - True if the timeline is paused
 # 
 # Make sure the corresponding toggle is enabled in the Execute DAT.
+import os
 
 def onStart():
 	return
@@ -14,19 +15,32 @@ def dialogChoice(choice):
 	return
 
 def onCreate():
+	current_dir_path = os.path.join(os.getcwd(), "MediaPipe.tox")
+	toxes_dir_path = os.path.join(os.getcwd(), "toxes", "MediaPipe.tox")
+
 	currentState = parent().par.enableexternaltox
 	if currentState==False:
-		op.TDResources.PopDialog.OpenDefault(
-			text='Check "Enable External tox" toggle to keep save times fast',
-			title='Externalise MediaPipe tox',
-			buttons=['Close', 'Help'],
-			callback=dialogChoice,
-			details='',
-			textEntry=False,
-			escButton=1,
-			enterButton=1,
-			escOnClickAway=True
-		)
+		# Check if the file exists at either location
+		if os.path.exists(current_dir_path):
+			parent().par.enableexternaltox = True
+			parent().par.externaltox = 'MediaPipe.tox'
+			print(f"MediaPipe.tox found at {current_dir_path}")
+		elif os.path.exists(toxes_dir_path):
+			parent().par.enableexternaltox = True
+			parent().par.externaltox = 'toxes/MediaPipe.tox'
+		else:
+			print("MediaPipe.tox not found in either location.")
+			op.TDResources.PopDialog.OpenDefault(
+				text='Check "Enable External tox" toggle to keep save times fast',
+				title='Externalise MediaPipe tox',
+				buttons=['Close', 'Help'],
+				callback=dialogChoice,
+				details='',
+				textEntry=False,
+				escButton=1,
+				enterButton=1,
+				escOnClickAway=False
+			)
 	return
 
 def onExit():
